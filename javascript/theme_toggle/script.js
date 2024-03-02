1046736428e799a1c6c18f45e462eee4f960c207
helena <helenaff13@gmail.com>
2024-03-02 20:45:36 -0300

    theme switch functionality added

--- javascript/theme_toggle/script.js
+++ javascript/theme_toggle/script.js
@@ -1,5 +1,18 @@
+const body = document.querySelector("body");
 const btnLightMode = document.querySelector(".btn-light");
+const btnDarkMode = document.querySelector(".btn-dark");
+const icon = document.querySelector(".theme-icon");
 
 btnLightMode.addEventListener('click', () => {
-    console.log("aaa");
+    body.classList.remove('dark');
+    btnLightMode.classList.add('btn-selected')
+    btnDarkMode.classList.remove('btn-selected')
+    icon.src = "images/sun.png";
+})
+
+btnDarkMode.addEventListener('click', () => {
+    body.classList.add('dark');
+    btnLightMode.classList.remove('btn-selected');
+    btnDarkMode.classList.add('btn-selected');
+    icon.src = "images/moon.png";
 })
