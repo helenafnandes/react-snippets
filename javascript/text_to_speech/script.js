5135b85322acc281bb09b11de03153d6b95083c6
helena <helenaff13@gmail.com>
2024-03-12 15:40:53 -0300

    text to speech - language options implemented

--- javascript/text_to_speech/script.js
+++ javascript/text_to_speech/script.js
@@ -3,13 +3,18 @@ const btnPause = document.querySelector(".btn-pause");
 const btnStop = document.querySelector(".btn-stop");
 const inputText = document.querySelector("#text");
 const inputSpeed = document.querySelector("#input-speed");
+const inputLanguage = document.querySelector("#input-language");
+
 
 btnPlay.addEventListener('click', () => {
-    playText(inputText.value);
-})
+    playText(inputText.value, inputLanguage.value);
+});
 
-function playText(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
+function playText(text, language) {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
     utterance.rate = inputSpeed.value || 1;
+    utterance.lang = inputLanguage.value;
+
     speechSynthesis.speak(utterance);
 }
