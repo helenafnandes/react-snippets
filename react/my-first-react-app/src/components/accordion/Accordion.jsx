96182c80218dcb59d8fb8ad6e43aa2e6248e86fe
helena <helenaff13@gmail.com>
2024-04-06 15:44:25 -0300

    accordion component (single and multi selection) done

---
+++ react/my-first-react-app/src/components/accordion/Accordion.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import data from './data';
+import './index.css';
+
+export default function Accordion() {
+  // single selection
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  // multiple selection
+  const [multiSelection, setMultiSelection] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  function handleSingleSelection(currentId) {
+    setSelectedItem(currentId === selectedItem ? null : currentId);
+  }
+
+  function handleMultiSelection(currentId) {
+    if (selectedItems.includes(currentId)) {
+      // If currentId is already in selectedItems, remove it
+      setSelectedItems(selectedItems.filter((id) => id !== currentId));
+    } else {
+      // If currentId is not in selectedItems, add it
+      setSelectedItems([...selectedItems, currentId]);
+    }
+  }
+
+  function clearSelectedItems() {
+    setSelectedItem(null);
+    setSelectedItems([]);
+  }
+
+  return (
+    <div className="wrapper">
+      <div>
+        {multiSelection ? (
+          <button
+            onClick={() => {
+              setMultiSelection(false);
+              clearSelectedItems();
+            }}
+          >
+            Turn on Singleselection
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setMultiSelection(true);
+              clearSelectedItems();
+            }}
+          >
+            Turn on MultiSelection
+          </button>
+        )}
+      </div>
+
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div
+              className="Item"
+              onClick={
+                multiSelection
+                  ? () => handleMultiSelection(dataItem.id)
+                  : () => handleSingleSelection(dataItem.id)
+              }
+              key={dataItem.id}
+            >
+              <div className="topic">
+                <h3>{dataItem.topic}</h3>
+                {selectedItem !== dataItem.id &&
+                !selectedItems.includes(dataItem.id) ? (
+                  <span>+</span>
+                ) : null}
+              </div>
+              {selectedItem === dataItem.id ||
+              selectedItems.includes(dataItem.id) ? (
+                <div className="content">{dataItem.content}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ react/my-first-react-app/src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    topic: 'Mercury',
+    content:
+      'Mercury is the smallest planet in the solar system and the closest to the Sun.',
+  },
+  {
+    id: 2,
+    topic: 'Venus',
+    content:
+      'Venus is the second planet from the Sun in the solar system and is known as the "Evening Star" due to its bright appearance in the night sky.',
+  },
+  {
+    id: 3,
+    topic: 'Earth',
+    content:
+      'Earth is the third planet from the Sun in the solar system and the only known planet to support life.',
+  },
+  {
+    id: 4,
+    topic: 'Mars',
+    content:
+      'Mars is the fourth planet from the Sun in the solar system and is known as the "Red Planet" due to its reddish coloration.',
+  },
+];
+
+export default data;
