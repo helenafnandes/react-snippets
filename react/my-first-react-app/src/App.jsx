289cf18154808df6be1b01a9a6c3f27d624dfe1b
helena <helenaff13@gmail.com>
2024-04-09 20:40:21 -0300

    image slider - getting json data done

--- react/my-first-react-app/src/App.jsx
+++ react/my-first-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +21,12 @@ function App() {
       <hr />
       <h2>Star Rating</h2>
       <StarRating noOfStars={5} />
+      <hr />
+      <h2>Image Slider</h2>
+      <ImageSlider
+        url={'https://picsum.photos/v2/list?page=1&limit=10'}
+        limit={'10'}
+      />
     </div>
   );
 }
---
+++ react/my-first-react-app/src/components/image-slider/index.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+export default function ImageSlider({ url, limit = 5 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData(url) {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      if (!response.ok) {
+        setErrorMsg('Network response was not ok');
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setLoading(false);
+      return data;
+    } catch (error) {
+      setErrorMsg(error.message);
+      setLoading(false);
+      return null;
+    }
+  }
+
+  async function getImageData() {
+    const imageData = await fetchData(url);
+    setImages(imageData);
+    console.log(imageData); // Check the fetched JSON data
+  }
+
+  useEffect(() => {
+    if (url !== '') getImageData();
+  }, [url]);
+
+  if (loading) {
+    return <div className="container">Loading</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error: {errorMsg}</div>;
+  }
+  return <div className="container">index</div>;
+}
