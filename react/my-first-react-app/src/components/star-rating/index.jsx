c4d4191e82aa6d54ca9913ea9c1886e1e51442d5
helena <helenaff13@gmail.com>
2024-04-08 23:05:46 -0300

    star rating effects applied

--- react/my-first-react-app/src/components/star-rating/index.jsx
+++ react/my-first-react-app/src/components/star-rating/index.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
 import './index.css';
 
-export default function StarRating(noOfStars = 5) {
+export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hovering, setHovering] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex !== 0) {
+      setRating(currentIndex); // Update rating only when currentIndex changes and it's not 0
+    }
+  }, [currentIndex]); // Set currentIndex as a dependency
 
   function handleClick(currentIndex) {
-    setRating(currentIndex);
+    setRating(0);
+    setCurrentIndex(currentIndex); // Update currentIndex when a star is clicked
   }
 
   function handleMouseMove(currentIndex) {
@@ -15,21 +23,27 @@ export default function StarRating(noOfStars = 5) {
   }
 
   function handleMouseLeave() {
-    setHovering(rating);
+    setHovering(0);
   }
 
   return (
     <div className="star-rating">
       {[...Array(noOfStars)].map((_, index) => {
-        index += 1;
+        const starIndex = index + 1;
         return (
           <FaStar
-            key={index}
-            onClick={() => handleClick(index)}
-            onMouseMove={() => handleMouseMove(index)}
-            onMouseLeave={() => handleMouseLeave()}
+            key={starIndex}
+            onClick={() => handleClick(starIndex)}
+            onMouseMove={() => handleMouseMove(starIndex)}
+            onMouseLeave={handleMouseLeave}
             size={40}
-            className={index <= (hovering || rating) ? 'active' : 'inactive'}
+            className={
+              starIndex <= rating
+                ? 'rated'
+                : starIndex <= hovering
+                  ? 'hovered'
+                  : ''
+            }
           />
         );
       })}
